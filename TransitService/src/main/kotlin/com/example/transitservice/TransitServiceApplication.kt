680f7d5bef11a88b2819dfd2ed0d23dcda4a1ebb
Russ098 <francescorusso2012@gmail.com>
2022-08-08 18:47:57 +0200

    First commit

---
+++ TransitService/src/main/kotlin/com/example/transitservice/TransitServiceApplication.kt
@@ -0,0 +1,12 @@
+package com.example.transitservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TransitServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<TransitServiceApplication>(*args)
+	//
+}
